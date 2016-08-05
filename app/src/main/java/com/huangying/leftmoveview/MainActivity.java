2128275b2d2a6fa81f5a1e7762bf00e88b52e622
hyying <1547179047@qq.com>
2016-08-05 14:00:13 +0800

    整理drawable id

--- app/src/main/java/com/huangying/leftmoveview/MainActivity.java
+++ app/src/main/java/com/huangying/leftmoveview/MainActivity.java
@@ -1,8 +1,9 @@
 package com.huangying.leftmoveview;
 
+import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -18,44 +19,23 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-//    private int[] images = {R.drawable.one, R.drawable.two, R.drawable.three, R.drawable.four, R.drawable.five,
-//            R.drawable.six, R.drawable.seven, R.drawable.eight, R.drawable.nine,
-//            R.drawable.ten, R.drawable.eleven, R.drawable.twelve, R.drawable.thirteen, R.drawable.fourteen,
-//            R.drawable.fifteen, R.drawable.sixteen, R.drawable.seventeen, R.drawable.eighteen,
-//            R.drawable.ninteen, R.drawable.twenty};
-
     private List<Integer> imgs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ListView lv = (ListView) View.inflate(this, R.layout.activity_main, null);
-        setContentView(lv);
+        setContentView(R.layout.activity_main);
+        ListView lv = (ListView) findViewById(R.id.listview);
         initImages();
         lv.setAdapter(new MoveViewAdapter());
     }
 
     private void initImages() {
-        imgs.add(R.drawable.one);
-        imgs.add(R.drawable.two);
-        imgs.add(R.drawable.three);
-        imgs.add(R.drawable.four);
-        imgs.add(R.drawable.five);
-        imgs.add(R.drawable.six);
-        imgs.add(R.drawable.seven);
-        imgs.add(R.drawable.eight);
-        imgs.add(R.drawable.nine);
-        imgs.add(R.drawable.ten);
-        imgs.add(R.drawable.eleven);
-        imgs.add(R.drawable.twelve);
-        imgs.add(R.drawable.thirteen);
-        imgs.add(R.drawable.fourteen);
-        imgs.add(R.drawable.fifteen);
-        imgs.add(R.drawable.sixteen);
-        imgs.add(R.drawable.seventeen);
-        imgs.add(R.drawable.eighteen);
-        imgs.add(R.drawable.ninteen);
-        imgs.add(R.drawable.twenty);
+        TypedArray taypedArray = getResources().obtainTypedArray(R.array.pic_drawables);
+        for (int i = 0; i < taypedArray.length(); i++) {
+            imgs.add(taypedArray.getResourceId(i, -1));
+        }
+        taypedArray.recycle();
     }
 
     class MoveViewAdapter extends BaseAdapter {
@@ -100,44 +80,52 @@ public class MainActivity extends ActionBarActivity {
             viewHolder.my_delete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    TranslateAnimation ta = new TranslateAnimation(
-                            Animation.RELATIVE_TO_SELF, 0,
-                            Animation.RELATIVE_TO_SELF, -1.0f,
-                            Animation.RELATIVE_TO_SELF, 0,
-                            Animation.RELATIVE_TO_SELF, 0);
-                    ta.setDuration(200);
-                    finalConvertView.startAnimation(ta);
-                    new Handler().postDelayed(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            imgs.remove(position);
-                            notifyDataSetChanged();
-                        }
-                    }, 200);
+                    doDeletAnim(finalConvertView, position);
                 }
             });
             viewHolder.my_top.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    TranslateAnimation ta = new TranslateAnimation(
-                            Animation.RELATIVE_TO_SELF, 0,
-                            Animation.RELATIVE_TO_SELF, 0,
-                            Animation.RELATIVE_TO_SELF, 0,
-                            Animation.RELATIVE_TO_SELF, -1.0f);
-                    ta.setDuration(200);
-                    viewHolder.mv.startAnimation(ta);
-                    new Handler().postDelayed(new Runnable() {
-
-                        @Override
-                        public void run() {
-                            imgs.add(0, imgs.remove(position));
-                            notifyDataSetChanged();
-                        }
-                    }, 200);
+                    doTopAnim(viewHolder, position);
                 }
             });
             return convertView;
+        }
+
+        private void doTopAnim(ViewHolder viewHolder, final int position) {
+            TranslateAnimation ta = new TranslateAnimation(
+                    Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, -1.0f);
+            ta.setDuration(200);
+            viewHolder.mv.startAnimation(ta);
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    imgs.add(0, imgs.remove(position));
+                    notifyDataSetChanged();
+                }
+            }, 200);
+        }
+
+        private void doDeletAnim(View finalConvertView, final int position) {
+            TranslateAnimation ta = new TranslateAnimation(
+                    Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, -1.0f,
+                    Animation.RELATIVE_TO_SELF, 0,
+                    Animation.RELATIVE_TO_SELF, 0);
+            ta.setDuration(200);
+            finalConvertView.startAnimation(ta);
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    imgs.remove(position);
+                    notifyDataSetChanged();
+                }
+            }, 200);
         }
     }
 
